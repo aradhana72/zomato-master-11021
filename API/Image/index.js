fb3bec9bdf111ddc94453943c2be03e902029c9e
aradhana72 <aradhanakund@gmail.com>
2021-12-29 19:07:23 +0530

    image api

--- API/Image/index.js
+++ API/Image/index.js
@@ -4,6 +4,8 @@ import multer from "multer";
 
 import {ImageModel} from "../../database/allModels";
 
+import {s3Upload} from "../../Utils/AWS/s3";
+
 const Router = express.Router();
 
 //MULTER CONFIG
@@ -18,3 +20,24 @@ Params       None
 Access       Public
 Method       POST
 */
+
+Router.post("/", upload.single("file"), async(req,res)=> {
+  try {
+    const file = req.file;
+    const bucketOptions = {
+      Bucket: "shapeaioctoberbatch123",
+      Key: file.originalname,
+      Body: file.buffer,
+      ContentType: file.mimetype,
+      ACL: "public-read"
+    };
+
+    const uploadImage = await s3Upload(bucketOptions);
+    return res.status(200).json({uploadImage});
+
+  } catch (error) {
+    return res.status(500).json({error: error.message});
+  }
+});
+
+export default Router;
